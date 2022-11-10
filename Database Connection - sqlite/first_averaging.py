4f0bdc21128687289118715ecf865a104c45ceee
Federico Moretta <federicomoretta97@gmail.com>
2022-11-10 12:33:55 +0100

    first_averaging started, div-by-zero encountered

--- Database Connection - sqlite/first_averaging.py
+++ Database Connection - sqlite/first_averaging.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 import os.path
 import statistics as st
 
@@ -55,8 +56,82 @@ Exhaust_Oil_ComDB     = pd.read_sql_query('SELECT * FROM ComDB WHERE "Substrate"
 Sewage_ComDB   = pd.read_sql_query('SELECT * FROM ComDB WHERE "Substrate" = "Sewage sludge"',         conn, index_col='index')
 Food_Ind_ComDB = pd.read_sql_query('SELECT * FROM ComDB WHERE "Substrate" = "Food industry sludges"', conn, index_col='index')
 
-# # data processing
-# cols = list(ComDB)
+# data processing
+cols = list(ComDB)
+
+# Manure
+Dairy_Manure_ComDB   = Dairy_Manure_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()
+Goat_Manure_ComDB    = Goat_Manure_ComDB.drop(   [cols[0], cols[1]], axis=1).T.to_numpy()
+Chicken_Manure_ComDB = Chicken_Manure_ComDB.drop([cols[0], cols[1]], axis=1).T.to_numpy()
+Sheep_Manure_ComDB   = Sheep_Manure_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()
+Pig_Manure_ComDB     = Pig_Manure_ComDB.drop(    [cols[0], cols[1]], axis=1).T.to_numpy()
+Sow_Manure_ComDB     = Sow_Manure_ComDB.drop(    [cols[0], cols[1]], axis=1).T.to_numpy()
+Chicken_Litter_ComDB = Chicken_Litter_ComDB.drop([cols[0], cols[1]], axis=1).T.to_numpy()
+
+# Agricultural waste
+Straw_ComDB        = Straw_ComDB.drop(       [cols[0], cols[1]], axis=1).T.to_numpy()
+Rice_Husk_ComDB    = Rice_Husk_ComDB.drop(   [cols[0], cols[1]], axis=1).T.to_numpy()
+Sugar_Beet_ComDB   = Sugar_Beet_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()
+Dry_Grass_ComDB    = Dry_Grass_ComDB.drop(   [cols[0], cols[1]], axis=1).T.to_numpy()
+Maize_ComDB        = Maize_ComDB.drop(       [cols[0], cols[1]], axis=1).T.to_numpy()
+Raw_Potato_ComDB   = Raw_Potato_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()
+Potato_Waste_ComDB = Potato_Waste_ComDB.drop([cols[0], cols[1]], axis=1).T.to_numpy()
+Yard_Waste_ComDB   = Yard_Waste_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()
+Bamboo_ComDB       = Bamboo_ComDB.drop(      [cols[0], cols[1]], axis=1).T.to_numpy()
+
+# Organic waste
+Food_Waste_ComDB      = Food_Waste_ComDB.drop(    [cols[0], cols[1]], axis=1).T.to_numpy()
+Fruit_Veggie_ComDB    = Fruit_Veggie_ComDB.drop(   [cols[0], cols[1]], axis=1).T.to_numpy()
+Whey_ComDB            = Whey_ComDB.drop(           [cols[0], cols[1]], axis=1).T.to_numpy()
+OFMSW_ComDB           = OFMSW_ComDB.drop(          [cols[0], cols[1]], axis=1).T.to_numpy()
+Fish_Waste_ComDB      = Fish_Waste_ComDB.drop(     [cols[0], cols[1]], axis=1).T.to_numpy()
+Slaughter_Res_ComDB   = Slaughter_Res_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()
+Vinegar_Res_ComDB     = Vinegar_Res_ComDB.drop(    [cols[0], cols[1]], axis=1).T.to_numpy()
+Precooked_Waste_ComDB = Precooked_Waste_ComDB.drop([cols[0], cols[1]], axis=1).T.to_numpy()
+Exhaust_Oil_ComDB     = Exhaust_Oil_ComDB.drop(    [cols[0], cols[1]], axis=1).T.to_numpy()
+
+# Sludges
+Sewage_ComDB   = Sewage_ComDB.drop(  [cols[0], cols[1]], axis=1).T.to_numpy()  
+Food_Ind_ComDB = Food_Ind_ComDB.drop([cols[0], cols[1]], axis=1).T.to_numpy()
+
+# drop 'Category' and 'Substrate' list object for dict creation
+cols = cols[2:]
+
+# Manure
+Dairy_Manure   = {str(cols[i]): np.nanmean(Dairy_Manure_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Goat_Manure    = {str(cols[i]): np.nanmean(Goat_Manure_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Chicken_Manure = {str(cols[i]): np.nanmean(Chicken_Manure_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Sheep_Manure   = {str(cols[i]): np.nanmean(Sheep_Manure_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Pig_Manure     = {str(cols[i]): np.nanmean(Pig_Manure_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Sow_Manure     = {str(cols[i]): np.nanmean(Sow_Manure_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Chicken_Litter = {str(cols[i]): np.nanmean(Chicken_Litter_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+
+# Agricultural waste
+Straw        = {str(cols[i]): np.nanmean(Straw_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Rice_Husk    = {str(cols[i]): np.nanmean(Rice_Husk_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Sugar_Beet   = {str(cols[i]): np.nanmean(Sugar_Beet_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Dry_Grass    = {str(cols[i]): np.nanmean(Dry_Grass_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Maize        = {str(cols[i]): np.nanmean(Maize_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Raw_Potato   = {str(cols[i]): np.nanmean(Raw_Potato_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Potato_Waste = {str(cols[i]): np.nanmean(Potato_Waste_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Yard_Waste   = {str(cols[i]): np.nanmean(Yard_Waste_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Bamboo       = {str(cols[i]): np.nanmean(Bamboo_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+
+# Organic waste
+Food_Waste      = {str(cols[i]): np.nanmean(Food_Waste_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Fruit_Veggie    = {str(cols[i]): np.nanmean(Fruit_Veggie_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Whey            = {str(cols[i]): np.nanmean(Whey_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+OFMSW           = {str(cols[i]): np.nanmean(OFMSW_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Fish_Waste      = {str(cols[i]): np.nanmean(Fish_Waste_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Slaughter_Res   = {str(cols[i]): np.nanmean(Slaughter_Res_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Vinegar_Res     = {str(cols[i]): np.nanmean(Vinegar_Res_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Precooked_Waste = {str(cols[i]): np.nanmean(Precooked_Waste_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+Exhaust_Oil     = {str(cols[i]): np.nanmean(Exhaust_Oil_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+
+# Sludges
+Sewage   = {str(cols[i]): np.nanmean(Sewage_ComDB[i], dtype='float32') for i in range(0, len(cols))}  
+Food_Ind = {str(cols[i]): np.nanmean(Food_Ind_ComDB[i], dtype='float32') for i in range(0, len(cols))}
+
 # Manure_SAD    = {str(cols[i]): st.mean(Manure_PAD[cols[i]].tolist())    for i in range (2, len(cols))}
 # AgriWaste_SAD = {str(cols[i]): st.mean(AgriWaste_PAD[cols[i]].tolist()) for i in range (2, len(cols))}
 # OrgWaste_SAD  = {str(cols[i]): st.mean(OrgWaste_PAD[cols[i]].tolist())  for i in range (2, len(cols))}
